2ad156f28052c06c6791bb8e53f4b6afa6a4c6b7
awills96 <vtalex2014@gmail.com>
2021-11-17 13:15:42 -0500

    Started sandwich generator

---
+++ message-generator.js
@@ -0,0 +1,19 @@
+const breads = ["Brioche", "Rye", "French", "Focaccia", "Lettuce Wrap"]
+const cheeses = ["American", "Swiss", "Provolone", "Gouda", "Pepper Jack"]
+const meats = ["Ham", "Turkey", "Roast Beef", "Salami", "Chicken"]
+
+const chooseRandomListItem = list => {
+    return list[(Math.floor(Math.random() * list.length))]
+}
+
+const generateRandomSandwich = () => {
+    let bread = chooseRandomListItem(breads);
+    if (bread !== "Lettuce Wrap"){
+        bread = bread + " bread";
+    } else {
+        bread = "a " + bread;
+    }
+    return `You got a sandwich with ${chooseRandomListItem(meats)} and ${chooseRandomListItem(cheeses)} on ${bread}!`
+}
+
+console.log(generateRandomSandwich());
